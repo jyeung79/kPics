31b014329f42b4eafb0f4c51a6e6e8a1bcf1a87b
Jeff Yeung <jyeung.eng@gmail.com>
2020-11-30 10:50:38 -0800

    Add support for env variables in RN

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
+  const presets = ['babel-preset-expo'];
+  const plugins = [
+    ['module:react-native-dotenv']
+  ];
+
   return {
-    presets: ['babel-preset-expo'],
+    presets,
+    plugins
   };
 };
