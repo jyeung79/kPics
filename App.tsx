94b9ca11dbd53d67e384cefe1cf0440e60140b96
Jeff Yeung <jyeung.eng@gmail.com>
2020-12-08 22:27:11 -0800

    Setup SafeAreaProvider to have correct statusbar functionality

--- App.tsx
+++ App.tsx
@@ -17,8 +17,8 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
+        <StatusBar style={'dark'} translucent={true}/>
         <Navigation colorScheme={colorScheme} />
-        <StatusBar />
       </SafeAreaProvider>
     );
   }
